3bc984e2fd9523072572e304c29b832a8e76b84d
reader-leader <arumerekenova@gmail.com>
2022-10-30 17:35:23 +0600

    Initial commit

---
+++ src/task/Worker.java
@@ -0,0 +1,16 @@
+package task;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class Worker extends Thread {
+    private final int p;
+
+    public Worker(int p) {
+        this.p = p;
+    }
+
+    public void run() {
+        System.out.println(p);
+    }
+}
